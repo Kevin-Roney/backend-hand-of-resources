ab2b62d8cdbbe5431707b1a29aa7f5affeb5df40
Kevin Roney <ktroney.pdx@gmail.com>
2022-06-17 09:33:01 -0700

    completed getAll test for societies

---
+++ __tests__/society.test.js
@@ -0,0 +1,18 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('backend-express-template routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  it('GET should return a list of societies', async () => {
+    const resp = await request(app).get('/societies');
+    expect(resp.status).toBe(200);
+    expect(resp.body[0].id).toBe('1');
+  });
+  afterAll(() => {
+    pool.end();
+  });
+});
