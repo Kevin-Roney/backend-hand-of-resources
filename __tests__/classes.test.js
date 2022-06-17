bc2d2186df23a9f2592d3c5620569960fccb3c99
Kevin Roney <ktroney.pdx@gmail.com>
2022-06-17 11:41:43 -0700

    completed getById test

--- __tests__/classes.test.js
+++ __tests__/classes.test.js
@@ -12,6 +12,12 @@ describe('backend-express-template routes', () => {
     expect(resp.status).toBe(200);
     expect(resp.body[0].id).toBe('1');
   });
+  it('GET/:id should return a single ffxiv class', async () => {
+    const resp = await request(app).get('/classes/1');
+    expect(resp.status).toBe(200);
+    expect(resp.body.id).toBe('1');
+    expect(resp.body.name).toEqual('Warrior');
+  });
   afterAll(() => {
     pool.end();
   });
