5bff82a747ae970c80be201b5cca7666c5a84e5c
Kevin Roney <ktroney.pdx@gmail.com>
2022-06-17 11:28:49 -0700

    wrote test for getAll classes

---
+++ __tests__/classes.test.js
@@ -0,0 +1,18 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('backend-express-template routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  afterAll(() => {
+    it('GET should return a list of ffxiv classes', async () => {
+      const resp = await request(app).get('/classes');
+      expect(resp.status).toBe(200);
+      expect(resp.body[0].id).toBe('1');
+    });
+    pool.end();
+  });
+});
