d2508d99cdd32a3fd47b903bb419bcabe3a7eefa
Kevin Roney <ktroney.pdx@gmail.com>
2022-06-17 14:14:36 -0700

    wrote getById test for ships

--- __tests__/ships.test.js
+++ __tests__/ships.test.js
@@ -12,6 +12,13 @@ describe('backend-express-template routes', () => {
     expect(resp.status).toBe(200);
     expect(resp.body[0].id).toBe('1');
   });
+  it('GET/:id should return a single ship', async () => {
+    const resp = await request(app).get('/ships/1');
+    expect(resp.status).toBe(200);
+    expect(resp.body.id).toBe('1');
+    expect(resp.body.name).toEqual('Sloop');
+    expect(resp.body.cannons).toEqual(2);
+  });
   afterAll(() => {
     pool.end();
   });
